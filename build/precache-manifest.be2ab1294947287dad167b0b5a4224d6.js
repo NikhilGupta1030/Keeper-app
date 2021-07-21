e2635118975a463ba969b750662846fac279326a
NikhilGupta <nikhilg398@gmail.com>
2021-07-21 16:59:12 +0530

    first commit

---
+++ build/precache-manifest.be2ab1294947287dad167b0b5a4224d6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad29f341056139c4f80cccfe94a6b97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e891aa20af5f1e5081",
+    "url": "/static/js/2.4e2a1378.chunk.js"
+  },
+  {
+    "revision": "c56fde2f9d3fbbcbbb37",
+    "url": "/static/js/main.db674fa4.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
